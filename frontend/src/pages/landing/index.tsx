ee3e4d9e665f7e7579e4b066c8e1d56c69c8b1f1
Will Jiang <willjiang2018@gmail.com>
2022-11-04 21:18:13 -0700

    frontend function

--- frontend/src/pages/landing/index.tsx
+++ frontend/src/pages/landing/index.tsx
@@ -4,6 +4,7 @@ import { useToast } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import { useAccount, useSigner, useProvider } from "wagmi";
 import { Contract } from "@ethersproject/contracts";
+import Button from "components/button";
 
 const LandingPage = () => {
 
@@ -18,6 +19,18 @@ const LandingPage = () => {
         <div className={styles.heading}>Mega AA Wallet</div>
         <div className={styles.description}>...</div>
         <div className={styles.cards}>
+          <Button variant="primary" size="large" className={styles.card}>
+            Create new Mega AA
+          </Button>
+          <Button variant="primary" size="large" className={styles.card}>
+            Add Security Module
+          </Button>
+          <Button variant="primary" size="large" className={styles.card}>
+            Delete
+          </Button>
+          <Button variant="primary" size="large" className={styles.card}>
+            Interact
+          </Button>
           ....
         </div>
       </div>
