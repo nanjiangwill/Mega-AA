d9e97da65b1808b5d375243ba09d3b04d48c86de
rabib <53358432+rabib@users.noreply.github.com>
2022-11-05 18:05:46 -0400

    simple origin wallet - but not building atm

---
+++ contracts/src/SimpleOriginWallet.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.17;
+
+import "./SimpleModule.sol";
+import "./interfaces/IOriginWallet.sol";
+import "@hyperlane/contracts/middleware/InterchainAccountRouter.sol";
+
+contract SimpleOriginWallet is IOriginWallet {
+
+    address public owner;
+    address public interchainAccountRouter;
+
+    function handleUserOp(UserOperation calldata userOp) external {
+        require(validateUserOp(userOp));
+        routeCalls(userOp.destinationDomain, userOp.calls);
+    }
+
+    function validateUserOp(UserOperation calldata userOp) internal view returns (bool) {
+        bytes32 userOpHash = keccak256(abi.encode(
+            userOp.sender,
+            userOp.nonce,
+            userOp.destinationDomain,
+            userOp.calls,
+            bytes32(0x00)
+            ));
+        SimpleModule.verifySignature(owner, userOpHash, userOp.signature);
+    }
+
+    function routeCalls(uint32 destinationDomain, Call[] calldata calls) internal {
+        InterchainAccountRouter(interchainAccountRouter).dispatch(destinationDomain, calls);
+    }
+}
