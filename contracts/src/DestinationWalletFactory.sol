a97d6ba21b7c821b354328d5404211bbae3506f4
rabib <53358432+rabib@users.noreply.github.com>
2022-11-05 19:52:17 -0400

    destination wallet

--- contracts/src/DestinationWalletFactory.sol
+++ contracts/src/DestinationWalletFactory.sol
@@ -1,3 +1,17 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.17;
 
+import "./DestinationWallet.sol";
+import "./interfaces/IDestinationWalletFactory.sol";
+
+contract DestinationWalletFactory is IDestinationWalletFactory {
+    address public immutable interchainAccount;
+    constructor(address _interchainAccount) {
+        interchainAccount = _interchainAccount;
+    }
+
+    function createWallet() external returns (address) {
+        require(msg.sender == interchainAccount);
+        return address(new DestinationWallet(interchainAccount));
+    }
+}
--- contracts/src/interfaces/IDestinationWalletFactory.sol
+++ contracts/src/interfaces/IDestinationWalletFactory.sol
@@ -2,5 +2,5 @@
 pragma solidity ^0.8.17;
 
 interface IDestinationWalletFactory {
-    function createWallet(address owner) external;
+    function createWallet() external returns (address);
 }
