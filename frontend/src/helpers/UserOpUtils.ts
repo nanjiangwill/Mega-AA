b3d0102331ebbd8b0c3f950d72245752c3e3c914
rabib <53358432+rabib@users.noreply.github.com>
2022-11-06 02:15:15 -0500

    more comments

--- frontend/src/helpers/UserOpUtils.ts
+++ frontend/src/helpers/UserOpUtils.ts
@@ -66,6 +66,8 @@ export function getUserOpHash (op: UserOperationStruct, chainId: number): string
 // chainId is the chainId of the origin chain (in our case, Mumbai)
 // the Signer class is abstract and cannot be instantiated directly
 // use a concrete sub-class of Signer like Wallet or JsonRpcSigner
+// the output of this function goes into the signature field of the userOp
+// (before signing the userOp, the signature field is '0x')
 export function signUserOp(signer: Signer, userOp: UserOperationStruct, chainId: number): Promise<string> {
     const message = getUserOpHash(userOp, chainId);
     return signer.signMessage(message);
