469b129289ab6223d527167bda32cc240ccd78db
roshan <roshan.palakkal@gmail.com>
2022-11-05 21:20:30 -0400

    feat: otp contracts

---
+++ contracts/src/otp/OTP.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.17;
+import "./ImmutableInclusionVerifier.sol";
+
+contract OTP {
+    bytes32 public merkleRoot;
+    mapping(bytes32 => bool) nullifiers;
+
+    constructor(bytes32 _merkleRoot){
+        merkleRoot = _merkleroot;
+    }
+
+    function verifySignature(bytes32 note, bytes32[] memory proof) external pure returns (bool) {
+        if(ImmutableInclusionVerifier.verifyMerkleProof(note, proof, merkleRoot)){
+            nullifiers[keccak256(note)] = true;
+            return true;
+        }
+        return false;
+    }
+}
